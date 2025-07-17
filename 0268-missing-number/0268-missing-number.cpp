cb2fd82f167a200401d5a917defe18da77ba6205
SIVASURIYA K <165550047+SIVASURIYAK@users.noreply.github.com>
2025-07-17 09:34:36 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.59%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();//TEST
+        int t=n*(n+1)/2;
+        int s=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            s+=nums[i];
+        }
+      
+        return t-s;
+        
+        
+        
+    
+    }
+};
